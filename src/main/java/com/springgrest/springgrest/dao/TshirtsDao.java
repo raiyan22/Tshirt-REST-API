e7ff529da3547e2ecb9d4749b65a4ba0e0b8fd0c
raiyan22 <raiyanislam058@gmail.com>
2023-01-29 02:04:23 +0600

    updated implementations

---
+++ src/main/java/com/springgrest/springgrest/dao/TshirtsDao.java
@@ -0,0 +1,9 @@
+package com.springgrest.springgrest.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springgrest.springgrest.entities.Tshirts;
+
+public interface TshirtsDao extends JpaRepository<Tshirts, Long>{
+
+}
--- src/main/java/com/springgrest/springgrest/services/TshirtServiceImpl.java
+++ src/main/java/com/springgrest/springgrest/services/TshirtServiceImpl.java
@@ -4,61 +4,108 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springgrest.springgrest.dao.TshirtsDao;
 import com.springgrest.springgrest.entities.Tshirts;
+
+//@Service
+//public class TshirtServiceImpl implements TshirtService {
+//	
+//	List<Tshirts> list;
+//	
+//	public TshirtServiceImpl() {
+//			list = new ArrayList<>();
+//			list.add( new Tshirts( 634673, "Black", "XL") );
+//			list.add( new Tshirts( 917362, "Yellow", "M") );
+//	}
+//
+//	@Override
+//	public List<Tshirts> getTshirts() {
+//		return list;
+//	}
+//
+//	@Override
+//	public Tshirts getTshirt(long tshirtId) {
+//
+//		Tshirts tshirt = null;
+//		for(Tshirts eachTshirt : list) {
+//			if( eachTshirt.getId() == tshirtId) {
+//				tshirt = eachTshirt;
+//				break;
+//			}
+//		}
+//		return tshirt;
+//	}
+//	
+//	@Override
+//	public Tshirts addTshirt(Tshirts tshirt) {
+//
+//		list.add(tshirt);
+//		return tshirt;
+//	}
+//
+//	@Override
+//	public Tshirts updateTshirt(Tshirts tshirt) {
+//		for(Tshirts t : list) {
+//			if(t.getId() == tshirt.getId() ) {
+//				t.setColor(tshirt.getColor());
+//				t.setSize(tshirt.getSize());
+//				break;
+//			}
+//		}
+//		return tshirt;
+//	}
+//
+//	@Override
+//	public void deleteTshirt(long tshirtId) {
+//		list = this.list.stream().filter(e->e.getId() != tshirtId).collect(Collectors.toList());
+//	}
+// 
+//}
+
 
 @Service
 public class TshirtServiceImpl implements TshirtService {
 	
-	List<Tshirts> list;
+	@Autowired
+	private TshirtsDao tshirtsDao;
+	
 	
 	public TshirtServiceImpl() {
-			list = new ArrayList<>();
-			list.add( new Tshirts( 634673, "Black", "XL") );
-			list.add( new Tshirts( 917362, "Yellow", "M") );
+		
 	}
 
 	@Override
 	public List<Tshirts> getTshirts() {
-		return list;
+		return tshirtsDao.findAll();
 	}
 
 	@Override
 	public Tshirts getTshirt(long tshirtId) {
 
-		Tshirts tshirt = null;
-		for(Tshirts eachTshirt : list) {
-			if( eachTshirt.getId() == tshirtId) {
-				tshirt = eachTshirt;
-				break;
-			}
-		}
-		return tshirt;
+		
+		return tshirtsDao.getOne(tshirtId);
 	}
 	
 	@Override
 	public Tshirts addTshirt(Tshirts tshirt) {
 
-		list.add(tshirt);
+		tshirtsDao.save(tshirt);
 		return tshirt;
 	}
 
 	@Override
 	public Tshirts updateTshirt(Tshirts tshirt) {
-		for(Tshirts t : list) {
-			if(t.getId() == tshirt.getId() ) {
-				t.setColor(tshirt.getColor());
-				t.setSize(tshirt.getSize());
-				break;
-			}
-		}
+		tshirtsDao.save(tshirt);
 		return tshirt;
 	}
 
 	@Override
 	public void deleteTshirt(long tshirtId) {
-		list = this.list.stream().filter(e->e.getId() != tshirtId).collect(Collectors.toList());
+		Tshirts tshirt = tshirtsDao.getOne(tshirtsId);
+		tshirtsDao.delete(tshirt);
 	}
  
 }
